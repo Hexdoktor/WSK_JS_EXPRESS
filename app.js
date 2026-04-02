12a11ba5df6157b2d4b78f479bd215954e5c5f23
Hexdoktor <savinainenjjj@gmail.com>
2026-04-02 13:40:26 +0300

    Complete Express Assignment 1

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-import express from "express";
-const hostname = "127.0.0.1";
+import express from 'express';
+const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Welcome to my REST API!");
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST API!');
 });
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+app.get('/api/v1/cats', (req, res) => {
+  const cat = {
+    cat_id: 1,
+    name: 'Mirri',
+    birthdate: '2020-05-12',
+    weight: 4.2,
+    owner: 'Juuso',
+    image: 'https://loremflickr.com/320/240/cat',
+  };
+
+  res.json(cat);
+});
+
+app.use('/public', express.static('public'));
